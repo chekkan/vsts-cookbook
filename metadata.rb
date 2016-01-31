e924812d2345803b200fdfa41fdfcd8f059f8465
Harish <harish_babu@outlook.com>
2016-01-31 19:41:05 +0000

    berks init and metadata file added

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'vsts'
+maintainer       'Harish Babu'
+maintainer_email 'harish_babu@outlook.com'
+license          'The MIT License'
+description      'Provide a set of vsts specific resources'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+supports         'windows'
+source_url       'https://github.com/chekkan/vsts-cookbook' if respond_to?(:source_url)
+issues_url       'https://github.com/chekkan/vsts-cookbook/issues' if respond_to?(:issues_url)
