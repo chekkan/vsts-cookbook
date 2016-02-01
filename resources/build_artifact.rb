9b790437778154510f1b8e019475c81626bf94e6
Harish <harish_babu@outlook.com>
2016-02-01 11:59:58 +0000

    added the remote_file resource for download

--- resources/build_artifact.rb
+++ resources/build_artifact.rb
@@ -4,6 +4,7 @@ property :instance, String, :required => true
 property :project_id, String, :required => true
 property :build_id, Fixnum, :required => true
 property :access_token, String, :require => true
+property :path, String, :required => true
 
 default_action :download
 
@@ -24,6 +25,17 @@ action :download do
 
   parsed = JSON.parse(res)
 
-  parsed['value'][0]['name']
+  idx = parsed['value'].find_index {|x| x == x['name'] == artifact_name}
+
+  artifact = parsed['value'][idx]
+  download_url = artifact['resource']['downloadUrl']
+
+  artifact_filename = "#{artifact_name}-#{build_id}"
+
+  remote_file "#{path}\\#{artifact_filename}" do
+    source download_url
+    action :create
+    headers( "Authorization" => "Bearer #{access_token}")
+  end
 
 end
